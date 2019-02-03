afefc3d3c93b02b03873680b05a65739703b4367
Travis Ho <travisho@huntsman-ve702-5327.apng.wlan.private.upenn.edu>
2019-02-03 12:29:20 -0500

    bar chart pulls from keyword ferquency on API

--- client/src/charts/BarWithStyles.js
+++ client/src/charts/BarWithStyles.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
 
-class BarWithTitle extends React.Component {
+class BarWithStyles extends React.Component {
   render() {
+    const styles = {
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    }
     return (
       <div>
         <h4>{this.props.title}</h4>
@@ -22,9 +29,9 @@ class BarWithTitle extends React.Component {
   }
 }
 
-BarWithTitle.defaultProps = {
+BarWithStyles.defaultProps = {
   title: "No Title",
   data: [],
 }
 
-export default BarWithTitle;
+export default BarWithStyles;
