a3e3b445f6e8bc28266be31fa36ea6cdc1dfe4fe
Wilson <wiyu98@gmail.com>
2019-02-02 20:53:37 -0500

    User and meeting models

--- server/serverapp/admin.py
+++ server/serverapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Profile, Meeting
 
-# Register your models here.
+admin.site.register(Profile, Meeting)
--- server/serverapp/models.py
+++ server/serverapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  #voice_sample
+
+class Meeting(models.Model):
+  date = models.DateField()
+  name = models.CharField(max_length = 128)
+
+  MEETING_CATEGORIES = (
+    'Brainstorming',
+    'Planning',
+    'Training',
+    'Status Updating',
+    'Decision Making',
+    'Problem Solving',
+    'Reporting',
+    'Other'
+  )
+  category = models.CharField(choices = MEETING_CATEGORIES)
+
