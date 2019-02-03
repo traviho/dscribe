05a67a3ba06f7ae55ea2a0109280cb5e27bb107c
Wilson <wiyu98@gmail.com>
2019-02-03 00:02:15 -0500

    Updated models with necessary fields

--- server/serverapp/models.py
+++ server/serverapp/models.py
@@ -7,9 +7,15 @@ class Profile(models.Model):
 
 class Meeting(models.Model):
   date = models.DateField()
-  name = models.CharField(max_length = 128)
+  name = models.CharField(max_length = 256)
+  description = models.TextField()
   #audio = models.FileField(upload_to='meetingaudio/')
   text = models.TextField()
+  keyText = models.TextField()
+  sentimentScore = models.DecimalField(max_digits = 2, decimal_places = 1)
+  sentimentMagnitude = models.DecimalField(max_digits = 2, decimal_places = 1)
+  wordCount = models.PositiveSmallIntegerField()
+  keyWordCount = models.PositiveSmallIntegerField()
 
   MEETING_CATEGORIES = [
     ('Brainstorming', 'Brainstorming'),
@@ -23,9 +29,22 @@ class Meeting(models.Model):
   ]
   category = models.CharField(choices = MEETING_CATEGORIES, max_length = 200)
 
-class MeetingMember(models.Model):
+class Attendee(models.Model):
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
   meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
   text = models.TextField()
+  keyText = models.TextField()
+  wordCount = models.PositiveSmallIntegerField()
+  keyWordCount = models.PositiveSmallIntegerField()
+  numQuestions = models.PositiveSmallIntegerField()
 
-# Models for results
+class Sentence(models.Model):
+  attendee = models.ForeignKey(Attendee, on_delete=models.CASCADE)
+  text = models.TextField()
+  keyText = models.TextField()
+  beginOffset = models.PositiveSmallIntegerField()
+  sentimentScore = models.DecimalField(max_digits = 2, decimal_places = 1)
+  sentimentMagnitude = models.DecimalField(max_digits = 2, decimal_places = 1)
+  wordCount = models.PositiveSmallIntegerField()
+  keyWordCount = models.PositiveSmallIntegerField()
+  question = models.BooleanField()
